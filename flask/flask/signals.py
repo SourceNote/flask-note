1af89df4d9fcbbbfd7b6eeb1857f57046c3e328c
Tacey <xinyong.wang@xtalpi.com>
2018-06-12 11:30:47 +0800

    flask中用于对象通信的信号

--- flask/flask/signals.py
+++ flask/flask/signals.py
@@ -1,57 +1,51 @@
 # -*- coding: utf-8 -*-
 """
-    flask.signals
-    ~~~~~~~~~~~~~
-
-    Implements signals based on blinker if available, otherwise
-    falls silently back to a noop.
-
-    :copyright: © 2010 by the Pallets team.
-    :license: BSD, see LICENSE for more details.
+    flask.signals：实现基于blinker的信号
 """
 
 signals_available = False
 try:
     from blinker import Namespace
+
     signals_available = True
 except ImportError:
     class Namespace(object):
         def signal(self, name, doc=None):
             return _FakeSignal(name, doc)
 
+
     class _FakeSignal(object):
-        """If blinker is unavailable, create a fake class with the same
-        interface that allows sending of signals but will fail with an
-        error on anything else.  Instead of doing anything on send, it
-        will just ignore the arguments and do nothing instead.
+
+        """
+        如果blinker不可用，创建一个具有相同接口的虚假类，忽略参数，
+        在发送信号的时候什么都不做，或者抛出一个异常
         """
 
         def __init__(self, name, doc=None):
             self.name = name
             self.__doc__ = doc
+
         def _fail(self, *args, **kwargs):
-            raise RuntimeError('signalling support is unavailable '
-                               'because the blinker library is '
-                               'not installed.')
+            raise RuntimeError('信号不可用'
+                               'blinker库没有安装 ')
+
         send = lambda *a, **kw: None
         connect = disconnect = has_receivers_for = receivers_for = \
             temporarily_connected_to = connected_to = _fail
         del _fail
 
-# The namespace for code signals.  If you are not Flask code, do
-# not put signals in here.  Create your own namespace instead.
+# 代码信号的命名空间。如果不是Flask代码，不应该将信号放在这里，
+# 需要创建自己的命名空间
 _signals = Namespace()
 
-
-# Core signals.  For usage examples grep the source code or consult
-# the API documentation in docs/api.rst as well as docs/signals.rst
-template_rendered = _signals.signal('template-rendered')
-before_render_template = _signals.signal('before-render-template')
-request_started = _signals.signal('request-started')
-request_finished = _signals.signal('request-finished')
-request_tearing_down = _signals.signal('request-tearing-down')
-got_request_exception = _signals.signal('got-request-exception')
-appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
-appcontext_pushed = _signals.signal('appcontext-pushed')
-appcontext_popped = _signals.signal('appcontext-popped')
-message_flashed = _signals.signal('message-flashed')
+# 核心信号
+template_rendered = _signals.signal('template-rendered')  # 模板渲染信号
+before_render_template = _signals.signal('before-render-template')  # 模板渲染前信号
+request_started = _signals.signal('request-started')  # 请求已经开始信号
+request_finished = _signals.signal('request-finished')  # 请求完成信号
+request_tearing_down = _signals.signal('request-tearing-down')  # 请求结束信号
+got_request_exception = _signals.signal('got-request-exception')  # 获得请求异常信号
+appcontext_tearing_down = _signals.signal('appcontext-tearing-down')  # 应用上下文结束信号
+appcontext_pushed = _signals.signal('appcontext-pushed')  # 应用上下文创建进栈信号
+appcontext_popped = _signals.signal('appcontext-popped')  # 应用上下文弹出栈信号
+message_flashed = _signals.signal('message-flashed')  # 消息已经闪现信号
